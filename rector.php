5110cb97f608e4f470185a561b9d62ceab025b6c
Marcel Marnitz <marcelmarnitz@googlemail.com>
2025-07-29 13:50:12 +0200

    Adds PHPStand and Rector to dev stack

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return RectorConfig::configure()
+    // register single rule
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class
+    ])
+    // here we can define, what prepared sets of rules will be applied
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true
+    )
+    ->withAttributesSets();
